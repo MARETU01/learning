56a4f791c2342743dc2eef771f4a369085c60353
luoding <2636064351@qq.com>
2026-07-01 15:04:36 +0800

    update

--- web_crawler_learning/colly/utils/aria2c.go
+++ web_crawler_learning/colly/utils/aria2c.go
@@ -1,3 +1,33 @@
 package utils
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 const aria2cPath = `C:\Users\26360\Desktop\Projects\Tools\aria2c\aria2c.exe`
+
+func DownloadMP4Witharia2c(savePath, videoURL string) {
+	fmt.Printf("开始下载 %s...\n", savePath)
+
+	// 构造ffmpeg命令
+	cmd := exec.Command(
+		aria2cPath,
+		"-x16",
+		"-s16",
+		"-o", savePath,
+		videoURL,
+	)
+
+	fmt.Println(cmd)
+
+	// 执行命令并等待完成
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Printf("下载 %s 失败: %v\n输出: %s\n", savePath, err, string(output))
+		return
+	}
+
+	fmt.Printf("%s 下载完成！\n", savePath)
+	fmt.Println("=====================================")
+}
